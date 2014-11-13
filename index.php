94cfc34ac6f47f75ac89c92cf6f6ec8a2f622dfe
Pavel Houžva <phouzva@gmail.com>
2014-11-13 14:23:51 +0100

    fix

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             ini_set('error_reporting', E_ALL);
             ini_set('display_errors', 1);
 
-            //require_once 'ritetag/init.php';
+            require_once 'vendor/autoload.php';
             /*
              * configuration
              */
