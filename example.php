59b0f12ea70c04ed3b9ab082a1cbc6be0bbed739
pavel <phouzva@gmail.com>
2015-12-10 14:26:22 +0100

    fix - example

--- example.php
+++ example.php
@@ -19,10 +19,10 @@
             if (file_exists('config.php')) {
                 require_once 'config.php';
                 $client = new \Ritetag\API\Client(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET);
-                echo ($client->hashtagDirectory("test")->getBody());
+                echo ($client->hashtagStats("test")->getBody());
                 echo "<hr>";
                 $client->returnRequest();
-                echo ($client->hashtagDirectory("test")->to_url());
+                echo ($client->hashtagStats("test")->to_url());
             } else {
                 ?>
                 <div class="alert alert-danger" role="alert">Missing configuration file config.php (you can copy config.sample.php and fill your auth tokens from <a href="https://ritetag.com/developer/dashboard">developers dashboard</a>)</div>
